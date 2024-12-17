623833ee8f6788870f67c9ad0c742c07bd4bdbd7
Roman Leshin <vorzee@gmail.com>
2024-12-17 07:16:33 +0500

    Добавили бизнес-логику в классе Game

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import lombok.Getter;
 import ru.netology.dto.Player;
+import ru.netology.exceptions.NotRegisteredException;
 
 import java.util.List;
 
@@ -11,14 +12,38 @@ public class Game {
     private List<Player> players;
 
     public boolean register (Player player) {
-        return false;
+        for (Player playerSource : players) {
+            if (playerSource.getId() == player.getId()) {
+                return false;
+            }
+        }
+        players.add(player);
+        return true;
     }
 
     public int round (String playerName1, String playerName2) {
-        return 0;
+        Player first = getByPlayerName(playerName1);
+        Player second = getByPlayerName(playerName2);
+        if (first == null || second == null) {
+            throw new NotRegisteredException();
+        }
+        switch (Integer.compare(first.getStrength(), second.getStrength())) {
+            case 1:
+                return 1;
+            case -1:
+                return 2;
+            default:
+                return 0;
+        }
     }
 
     public Player getByPlayerName (String playerName) {
-        return null;
+        Player result = null;
+        for (Player player : players) {
+            if (player.getName().equals(playerName)) {
+                result = player;
+            }
+        }
+        return result;
     }
 }
